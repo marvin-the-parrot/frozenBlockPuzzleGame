14c46bc7919de1cd2ec7562571a81d44386f5e35
Marvin Kleinlehner <e12024734@student.tuwien.ac.at>
2023-06-06 17:56:00 +0200

    added jumping

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -7,6 +7,11 @@ public class playerController : MonoBehaviour {
     public float moveSpeed = 5f;        // Player movement speed
     public float rotationSpeed = 10f;   // Player rotation speed
 
+    public float gravityScale = 2f; // Controls the falling speed
+
+    public float jumpForce = 5f; // The force applied to make the player jump
+    private bool isJumping = false; // Flag to check if the player is jumping
+
     private Rigidbody rb;
     private Vector3 movementInput;
     private float turnInput;
@@ -20,13 +25,30 @@ public class playerController : MonoBehaviour {
         float moveZ = Input.GetAxis("Vertical");
         float mouseX = Input.GetAxis("Mouse X");
 
+        if (Input.GetButtonDown("Jump") && !isJumping) {
+            Jump();
+        }
+
         // Calculate movement input
         movementInput = new Vector3(0f,0f,moveZ);
 
         turnInput = mouseX;
     }
 
+    private void Jump() {
+        rb.velocity = Vector3.zero;
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isJumping = true;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isJumping = false;
+        }
+    }
+
     private void FixedUpdate() {
+        rb.AddForce(Physics.gravity * gravityScale, ForceMode.Acceleration);
         // Move the player
         MovePlayer();
 
